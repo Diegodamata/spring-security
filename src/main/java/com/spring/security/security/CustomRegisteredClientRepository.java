eb08ec632d72f0c859d176e7dabe8902b799a307
Diego da Mata Pontes <Diegodamata>
2025-03-16 13:38:02 -0300

    Criando um RegisteredClientsRepository customizado

---
+++ src/main/java/com/spring/security/security/CustomRegisteredClientRepository.java
@@ -0,0 +1,48 @@
+package com.spring.security.security;
+
+import com.spring.security.services.ClientService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.stereotype.Component;
+
+//uma classe de implementação da interface RegisteredClientRepository que é uma interface do authorization server
+//que serve para gerenciar os clientes registrados que integarem com o servidor de autorização
+
+@Component
+@RequiredArgsConstructor
+public class CustomRegisteredClientRepository implements RegisteredClientRepository {
+
+    private final ClientService clientService;
+
+    @Override
+    public void save(RegisteredClient registeredClient) {}
+
+    @Override
+    public RegisteredClient findById(String id) {
+        return null;
+    }
+
+    @Override
+    public RegisteredClient findByClientId(String clientId) {
+
+        var client = clientService.obterClientPorId(clientId);
+
+        if(client == null){
+            return null;
+        }
+
+        return RegisteredClient
+                .withId(client.getId().toString())
+                .clientId(client.getClientId())
+                .clientSecret(client.getClientSecret())
+                .redirectUri(client.getRedirectURI())
+                .scope(client.getScope())
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+                .build();
+    }
+}
--- src/main/java/com/spring/security/services/ClientService.java
+++ src/main/java/com/spring/security/services/ClientService.java
@@ -3,6 +3,7 @@ package com.spring.security.services;
 import com.spring.security.models.Client;
 import com.spring.security.repositories.ClientRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,8 +11,10 @@ import org.springframework.stereotype.Service;
 public class ClientService {
 
     private final ClientRepository repository;
+    private final PasswordEncoder encoder;
 
     public Client salvar(Client client){
+        client.setClientSecret(encoder.encode(client.getClientSecret()));
         return repository.save(client);
     }
 
